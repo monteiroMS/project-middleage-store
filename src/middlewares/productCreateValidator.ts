3a3b15df93a31ec5d32d92b06e49b51b54ea135c
Matheus Monteiro <matheuschran@hotmail.com>
2022-09-19 16:53:00 -0300

    Requisito 6 - Validações para criação de produto no endpoint POST /products

--- src/middlewares/productCreateValidator.ts
+++ src/middlewares/productCreateValidator.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 
 const schema = Joi.object({
-  name: Joi.string().required(),
-  amount: Joi.string().required(),
+  name: Joi.string().min(3).required(),
+  amount: Joi.string().min(3).required(),
 });
 
 const productCreateValidator = (req: Request, res: Response, next: NextFunction) => {
@@ -11,7 +11,9 @@ const productCreateValidator = (req: Request, res: Response, next: NextFunction)
 
   if (validation.error) {
     const [{ message }] = validation.error.details;
-    return res.status(400).json({ message });
+    return message.includes('is required')
+      ? res.status(400).json({ message })
+      : res.status(422).json({ message });
   }
 
   next();
